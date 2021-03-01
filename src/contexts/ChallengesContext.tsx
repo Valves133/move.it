9ce9fc4602d0bb40f2e0621dca2eff5130b78e11
Victor Hugo Mendes Alves <victor.h.m.alves@gmail.com>
2021-03-01 02:17:27 +0100

    Function all buttons and reset

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -17,6 +17,7 @@ interface ChallengeContextData{
   resetChallenge: () => void;
   levelUp: () => void;
   startNewChallenge: () => void;
+  completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -49,6 +50,27 @@ export function ChallengesProvider({ children }:ChallengesProviderProps ){
     setActiveChallenge(null);
   }
 
+  function completeChallenge(){
+    if(!activeChallenge){
+      return;
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if( finalExperience>= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+  }
+
+
+
   return(
     <ChallengesContext.Provider 
     value={{
@@ -59,7 +81,8 @@ export function ChallengesProvider({ children }:ChallengesProviderProps ){
       startNewChallenge,
       activeChallenge,
       resetChallenge,
-      experienceToNextLevel
+      experienceToNextLevel,
+      completeChallenge
       }}>
       {children}
     </ChallengesContext.Provider>
